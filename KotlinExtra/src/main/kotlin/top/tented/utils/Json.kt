cdf0203197e7c9dc3d208608b0983d1b0ea66542
LimbolRain <limbolrain@gmail.com>
2018-03-09 03:55:42 +0800

    [ Script ] Nothing

--- KotlinExtra/src/main/kotlin/top/tented/utils/Json.kt
+++ KotlinExtra/src/main/kotlin/top/tented/utils/Json.kt
@@ -1,5 +1,8 @@
 package top.tented.utils
 
+import com.google.gson.Gson
+import java.io.Reader
+
 class JsonObject {
     val pairs = HashMap<String, Any?>()
 
@@ -14,8 +17,11 @@ class JsonObject {
             |"$key":${if(value is CharSequence) "\"$value\"" else value},
             """.trimMargin().run(::append)
         }
-    }.run { substring(0, length - 2) } + "}"
+    }.run { substring(0, length - 1) } + "}"
 }
 
 fun obj(body : JsonObject.() -> Unit) = JsonObject().apply(body)
 fun json(body: JsonObject.() -> Unit) = obj(body)
+
+inline fun <reified T> Gson.fromJson(json : String) : T = fromJson(json, T::class.java)
+inline fun <reified T> Gson.fromJson(reader : Reader) : T = fromJson(reader, T::class.java)
