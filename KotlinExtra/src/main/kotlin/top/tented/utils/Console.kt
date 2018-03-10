fc0cf9d3b22793a619078f61893be165e50fcfcb
LimbolRain <limbolrain@gmail.com>
2018-03-11 02:51:49 +0800

    [ Stupid ] Oh!I forget to commit gradle script file

--- KotlinExtra/src/main/kotlin/top/tented/utils/Console.kt
+++ KotlinExtra/src/main/kotlin/top/tented/utils/Console.kt
@@ -1,3 +1,7 @@
 package top.tented.utils
 
 fun printf( message : String , vararg args : Any? ) = System.out.printf(message, * args)
+
+fun printErr(message : Any?) = System.err.print(message)
+fun printlnErr(message : Any?) = System.err.println(message)
+fun printfErr(message : String, vararg args : Any?) = System.err.printf(message, * args)
