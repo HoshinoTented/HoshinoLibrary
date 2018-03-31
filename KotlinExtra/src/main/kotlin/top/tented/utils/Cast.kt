9b6f826469b78cc95de7111965e6e2ba3eff1b7c
LimbolRain <limbolrain@gmail.com>
2018-03-31 11:52:04 +0800

    fixed some bug code

--- KotlinExtra/src/main/kotlin/top/tented/utils/Cast.kt
+++ KotlinExtra/src/main/kotlin/top/tented/utils/Cast.kt
@@ -2,7 +2,6 @@
 
 package top.tented.utils
 
-fun <R> Any?.cast(lambda : Any?.() -> R) = lambda()
-fun <R> Any?.cast() = this as? R
-fun <R> Any?.uncheckCast() = this as R
+inline fun <reified R> Any?.cast() = this as? R
+inline fun <reified R> Any?.uncheckCast() = this as R
 inline fun <reified R> Any?.instanceOf() = this is R
